30e542587dec181df524332104534c4fd160c6f5
Mathew <mathew97.ho69@gmail.com>
2020-12-26 01:54:29 +1100

    Create Article Model

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Article(models.Model):
+  title = models.CharField(max_length=100, default='title')
+  content = models.TextField()
+  pub_date = models.DateTimeField(default=timezone.now)
