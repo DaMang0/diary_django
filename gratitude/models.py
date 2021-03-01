f92c70e8cb617fc4c409bbd2e5a0c82e31674da6
Mathew <mathew97.ho69@gmail.com>
2021-03-01 22:29:47 +1100

    Gratitude date page & Journal List page design refactor

--- gratitude/models.py
+++ gratitude/models.py
@@ -5,6 +5,7 @@ class Gratitude_List(models.Model):
   title = models.CharField(max_length=255, unique=True)
   user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
   date_created = models.DateField(auto_now_add=True)
+  time_created = models.DateTimeField(auto_now_add=True, null=True)
   edited = models.DateTimeField(auto_now=True)
 
   def __str__(self):
